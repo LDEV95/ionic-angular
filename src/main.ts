23b3468934efddd08c348a832c31c50801cfbe1b
LDEV95 <lawrencefanutzadavis@gmail.com>
2023-11-02 12:10:41 +0000

    started ionic angular project

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
+
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+// Call the element loader before the bootstrapModule/bootstrapApplication call
+defineCustomElements(window);
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
